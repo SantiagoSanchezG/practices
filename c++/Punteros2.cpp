184d61d8e6ce05eef6238c431a237b735e20003c
Santiago <sanchezgrisaless@gmail.com>
2021-11-10 06:41:31 -0500

    Punteros2 Completed

--- c++/Punteros2.cpp
+++ c++/Punteros2.cpp
@@ -1,19 +1,11 @@
 #include<iostream>
 
-int main(int p, int *d){
-    std::cout<<"Positions number: ";
-    std::cin>>p;
-    int n[p];
-    d = n;
-    for(int i = 0; i == n.size(); i++){
-        std::cout<<"\nValor of position: "<<i;
-        std::cin>>n[i];
-        if(p < *d){
-            p = n[i];
-        }
-        d++;
-    }
-    std::cout<<p;
+int main(int p, int *d, int a, int *b){
+    std::cout<<"Positions number: "; std::cin>>p; int n[p]; d = n;
+    for(int i = 0; i < p; i++){
+        std::cout<<"\nValor of position "<<i<<": "; std::cin>>n[i];
+        if(i == 0){a = *d; b = d;}else if(a > *d){a = *d; b = d;} d++;}
+    std::cout<<"\nThe smallest number is: "<<a<<" in the position: "<<b<<std::endl;
 
     return 0;
 }
